1ee5c7c5516eb3389a2fc3d92e2b127f61ecf391
Milad Ghasemebeigi <miladghasembeigi@gmail.com>
2025-12-03 09:52:03 +1100

    test: Initialise test setup

---
+++ tests/ReplacementSet.test.ts
@@ -0,0 +1,10 @@
+import { describe ,test,it , expect } from "vitest";
+import NaturalReplacementSet from "../src/mask/NaturalReplacementSet";
+
+
+
+describe("intialiseReplacementSet", () => {
+    test('JSON file should be parsed as WordMap from constructor', () => {
+
+    })
+});
