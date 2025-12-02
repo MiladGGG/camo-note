30f5e99ceefe32af4d276bcfcd0c9657efd24170
Milad Ghasemebeigi <miladghasembeigi@gmail.com>
2025-12-03 09:15:13 +1100

    feature: Initalise React project files

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import wordd from "./mask/NaturalReplacementSet"
+import './App.css'
+
+function App() {
+  wordd == null;
+  return (
+    <>
+      <h1>hi</h1>
+    </>
+  )
+}
+
+export default App
