8b472f82ffb545757844587f65488fab35473dc1
Milad Ghasemebeigi <miladghasembeigi@gmail.com>
2025-12-03 10:06:09 +1100

    test: Add initialisation tests for WordMap

--- tests/ReplacementSet.test.ts
+++ tests/ReplacementSet.test.ts
@@ -1,10 +1,16 @@
 import { describe ,test,it , expect } from "vitest";
 import NaturalReplacementSet from "../src/mask/NaturalReplacementSet";
+import ReplacementSet from "../src/mask/ReplacementSet";
 
 
 
 describe("intialiseReplacementSet", () => {
     test('JSON file should be parsed as WordMap from constructor', () => {
+        const wordSet = new NaturalReplacementSet().lengthMap;
 
+        console.log(wordSet);
+
+        expect(wordSet).not.toBeNull();
+        expect(wordSet).toBeInstanceOf(Map<Number, Set<String>>);
     })
 });
