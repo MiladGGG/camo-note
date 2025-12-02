71ee6cf7b7a02323d5618befaeafad7e4f38ae94
Milad Ghasemebeigi <miladghasembeigi@gmail.com>
2025-12-03 10:42:11 +1100

    test: Add TDD test for text masking

--- tests/ReplacementSet.test.ts
+++ tests/ReplacementSet.test.ts
@@ -1,16 +1,47 @@
-import { describe ,test,it , expect } from "vitest";
+import { describe ,test,it , expect, beforeAll } from "vitest";
 import NaturalReplacementSet from "../src/mask/NaturalReplacementSet";
 import ReplacementSet from "../src/mask/ReplacementSet";
 
 
+const replacementSet : ReplacementSet = new NaturalReplacementSet();
+
+beforeAll(() => {
+
+});
+
 
 describe("intialiseReplacementSet", () => {
     test('JSON file should be parsed as WordMap from constructor', () => {
-        const wordSet = new NaturalReplacementSet().lengthMap;
+        const wordSet = replacementSet.lengthMap
 
         console.log(wordSet);
 
+        // Assert exists and is type of WordMap
         expect(wordSet).not.toBeNull();
         expect(wordSet).toBeInstanceOf(Map<Number, Set<String>>);
+    })
+});
+
+
+describe("generateMask", () => {
+    test('Input words should be masked with words of same length', () => {
+        const input = "I am so mad";
+        const split : string[] = input.split(" ");
+
+        const replacement : string[] = replacementSet.generateMasked(input);
+
+        // Assert same length array
+        expect(replacement.length).toStrictEqual(split.length);
+        for (const index in replacement) {
+            const newWord = replacement[index];
+            const oldWord = split[index]
+
+            // Check new word is different
+            expect(newWord).not.toBe(oldWord);
+
+            // Check new word is same length
+            expect(newWord.length).toStrictEqual(oldWord.length);
+        }
+
     })
 });
