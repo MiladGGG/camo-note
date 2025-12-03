991c54063b0f7cf50188488a1ba626f92fef2ba4
Milad Ghasemebeigi <miladghasembeigi@gmail.com>
2025-12-03 11:07:20 +1100

    feature: Add basic word masking method

--- src/mask/ReplacementSet.ts
+++ src/mask/ReplacementSet.ts
@@ -1,5 +1,5 @@
 type rawMap =  Record<string, string[]>;
-type WordMap = Map<Number, Set<String>>;
+type WordMap = Map<Number, string[]>;
 
 abstract class ReplacementSet {
     private _lengthMap : WordMap;
@@ -8,11 +8,11 @@ abstract class ReplacementSet {
         this._lengthMap = this.initialiseReplacementSet(wordSet);
     }
 
-    private initialiseReplacementSet(words : rawMap) : Map<Number,Set<String>> {
+    private initialiseReplacementSet(words : rawMap) : WordMap {
         const newMap : WordMap = new Map();
         let rawWordSet = Object.entries(words); 
-        rawWordSet.forEach(([key, value]) => { //json to arra
-            newMap.set(Number(key), new Set(value))
+        rawWordSet.forEach(([key, value]) => {
+            newMap.set(Number(key), value)
         });
 
         return newMap
@@ -22,7 +22,29 @@ abstract class ReplacementSet {
         return this._lengthMap;
     }
 
-    //abstract handleNumber(length : number) : string;
+
+    private maskWord(word : string) : string {
+        const stringLength = word.length
+        const targetWords : string[] | undefined = this._lengthMap.get(stringLength);
+        if (targetWords === undefined) {
+            return "ERROR_PLACEHOLDER";
+        }
+
+        const randomValue = Math.floor(Math.random() * (targetWords.length -1));
+        let randomReplacement = targetWords[randomValue];
+
+        return randomReplacement;
+    }
+
+
+    public generateMasked(original : string[]) : string[] {
+        const maskedArray = new Array(original.length);
+        original.forEach((oldWord, index) => {
+            maskedArray[index] = this.maskWord(oldWord);
+        });
+
+        return maskedArray;
+    }
 
 
 }
